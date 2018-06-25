bdd891eb22db465bedb62672e2eb0f40fb0a1edd
Tim Anema <tim.anema@shopify.com>
2018-06-25 18:18:29 -0400

    original solution

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -8,31 +8,46 @@ import (
 	"github.com/gotoronto/rnd-workshop/crawler/web"
 )
 
+const concurrencyLimit = 20
+
 type Crawler struct {
 	list *lists.URLList
+	urls chan string
 }
 
 func New(seeds ...string) *Crawler {
 	return &Crawler{
 		list: lists.NewURLList(seeds...),
+		urls: make(chan string, concurrencyLimit),
 	}
 }
 
 func (crawler *Crawler) Crawl() {
 	fmt.Println("Starting crawler")
-	urlToCrawl, moreUrls := crawler.list.Pop()
-	for moreUrls {
-		links, err := web.Scrape(urlToCrawl)
-		if err != nil {
-			log.Printf("Error crawling %s: %v\n", urlToCrawl, err)
-			continue
-		}
+	url, more := crawler.list.Pop()
+	for more {
+		crawler.urls <- url
+		url, more = crawler.list.Pop()
+	}
 
-		for _, newURL := range links {
-			if ok, _ := crawler.list.Add(newURL); ok {
-				log.Printf("Found new url: %s", newURL)
-			}
+	for {
+		select {
+		case url := <-crawler.urls:
+			go crawler.crawl(url)
 		}
-		urlToCrawl, moreUrls = crawler.list.Pop()
+	}
+}
+
+func (crawler *Crawler) crawl(url string) {
+	links, err := web.Scrape(url)
+	if err != nil {
+		log.Printf("Error crawling %s: %v\n", url, err)
+		return
+	}
+
+	for _, newURL := range links {
+		if ok, _ := crawler.list.Add(newURL); ok {
+			crawler.urls <- newURL
+		}
 	}
 }
