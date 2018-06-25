e281e67eb19e3bbbc8abbcac64c88215981ec70e
Tim Anema <tim.anema@shopify.com>
2018-06-25 16:56:34 -0400

    Simplified original starting point

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -2,15 +2,10 @@ package crawler
 
 import (
 	"fmt"
-	"io"
 	"log"
-	"net/http"
-	"net/url"
-	"time"
-
-	"golang.org/x/net/html"
 
 	"github.com/gotoronto/rnd-workshop/crawler/lists"
+	"github.com/gotoronto/rnd-workshop/crawler/web"
 )
 
 type Crawler struct {
@@ -25,70 +20,19 @@ func New(seeds ...string) *Crawler {
 
 func (crawler *Crawler) Crawl() {
 	fmt.Println("Starting crawler")
-
 	urlToCrawl, moreUrls := crawler.list.Pop()
 	for moreUrls {
-		body, err := get(urlToCrawl)
+		links, err := web.Scrape(urlToCrawl)
 		if err != nil {
 			log.Printf("Error crawling %s: %v\n", urlToCrawl, err)
 			continue
 		}
 
-		for _, newURL := range parseLinks(body) {
+		for _, newURL := range links {
 			if ok, _ := crawler.list.Add(newURL); ok {
 				log.Printf("Found new url: %s", newURL)
 			}
 		}
-
-		err = body.Close()
-		if err != nil {
-			log.Printf("Failed to close body: %v\n", err)
-		}
-
 		urlToCrawl, moreUrls = crawler.list.Pop()
-	}
-}
-
-func get(url string) (io.ReadCloser, error) {
-	log.Printf("Visiting %s", url)
-	client := http.Client{
-		Timeout: time.Duration(2 * time.Second),
-	}
-	response, err := client.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	return response.Body, nil
-}
-
-func parseLinks(r io.Reader) []string {
-	tk := html.NewTokenizer(r)
-	var links []string
-	for {
-		tt := tk.Next()
-
-		switch tt {
-		case html.ErrorToken:
-			return links
-		case html.StartTagToken:
-			t := tk.Token()
-			if t.Data != "a" {
-				break
-			}
-			for _, a := range t.Attr {
-				if a.Key != "href" {
-					continue
-				}
-
-				parsed, err := url.ParseRequestURI(a.Val)
-				if err != nil || !parsed.IsAbs() {
-					continue
-				}
-
-				fmt.Println(parsed)
-				links = append(links, parsed.String())
-				break
-			}
-		}
 	}
 }
---
+++ crawler/web/scraper.go
@@ -0,0 +1,60 @@
+package web
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
+
+	"golang.org/x/net/html"
+)
+
+// Scrape will fetch an html page and scrape it for links. These links will be
+// returned as a string array. If there was an error while requesting it will be
+// returned as well
+func Scrape(url string) ([]string, error) {
+	log.Printf("Visiting %s", url)
+	client := http.Client{
+		Timeout: time.Duration(2 * time.Second),
+	}
+	response, err := client.Get(url)
+	if err != nil {
+		return []string{}, err
+	}
+	defer response.Body.Close()
+	return parseLinks(response.Body), nil
+}
+
+func parseLinks(r io.Reader) []string {
+	tk := html.NewTokenizer(r)
+	var links []string
+	for {
+		tt := tk.Next()
+
+		switch tt {
+		case html.ErrorToken:
+			return links
+		case html.StartTagToken:
+			t := tk.Token()
+			if t.Data != "a" {
+				break
+			}
+			for _, a := range t.Attr {
+				if a.Key != "href" {
+					continue
+				}
+
+				parsed, err := url.ParseRequestURI(a.Val)
+				if err != nil || !parsed.IsAbs() {
+					continue
+				}
+
+				fmt.Println(parsed)
+				links = append(links, parsed.String())
+				break
+			}
+		}
+	}
+}
