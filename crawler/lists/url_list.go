2801b905a38438ce8f8f830424483a382649fff7
Tim Anema <tim.anema@shopify.com>
2018-06-29 15:50:29 -0400

    Change check method name

--- crawler/lists/url_list.go
+++ crawler/lists/url_list.go
@@ -19,15 +19,15 @@ func NewURLList(seeds ...string) *URLList {
 
 // Add will add a url to the list if it is not already in the list
 func (list *URLList) Add(url string) (bool, error) {
-	if ok, _ := list.Check(url); ok {
+	if ok, _ := list.Find(url); ok {
 		return false, errors.New("url already exists")
 	}
 	list.URLs = append(list.URLs, url)
 	return true, nil
 }
 
-// Check will check if the url exists already in the list
-func (list *URLList) Check(url string) (bool, int) {
+// Find will check if the url exists already in the list
+func (list *URLList) Find(url string) (bool, int) {
 	for i, u := range list.URLs {
 		if u == url {
 			return true, i
@@ -38,7 +38,7 @@ func (list *URLList) Check(url string) (bool, int) {
 
 // Delete will remove a url from the list
 func (list *URLList) Delete(url string) bool {
-	if ok, i := list.Check(url); ok {
+	if ok, i := list.Find(url); ok {
 		list.URLs = append(list.URLs[:i], list.URLs[i+1:]...)
 		return true
 	}
