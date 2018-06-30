1bf383caf0f2731c6752261766d5699d6f2aa40d
Jessica Xie <jessica.xie@shopify.com>
2018-06-30 12:23:46 -0400

    Add resources and steps on running tests

--- crawler/crawler_test.go
+++ crawler/crawler_test.go
@@ -6,11 +6,16 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// TODO: expand the description of the test in a comment
-// TODO: add "SOLUTION:" before each solution
-// TODO: add a blurb about cd into the repo and how to run test
-// TODO: add go dep
+// To run the test
+// dep ensure 		// to install dependencies
+// go test ./...
 
+// Resources:
+// https://gobyexample.com/
+// https://golang.org/doc/ (use the search bar for doc)
+// Google and Stack Overflow are your friends :)
+
+// SOLUTION:
 // func Add(url string) {
 // 	URLs = append(URLs, url)
 // }
@@ -21,6 +26,7 @@ func TestAddANewURL(t *testing.T) {
 	assert.Equal(t, []string{"http://www.google.ca"}, URLs)
 }
 
+// SOLUTION:
 // func Add(url string) {
 // 	for _, u := range URLs {
 // 		if u == url {
@@ -40,6 +46,7 @@ func TestAddOnlyKeepsUniqueURL(t *testing.T) {
 
 // doc on deleting an element from an array: https://github.com/golang/go/wiki/SliceTricks#delete
 
+// SOLUTION:
 // func Delete(url string) {
 // 	var i int
 // 	var u string
@@ -58,6 +65,7 @@ func TestDeleteURL(t *testing.T) {
 	assert.Equal(t, []string{}, URLs)
 }
 
+// SOLUTION:
 // func Delete(url string) {
 // 	var i int
 // 	var u string
@@ -81,6 +89,7 @@ func TestDeleteDoesNothingIfURLNotExist(t *testing.T) {
 	assert.Equal(t, []string{"http://www.amazon.ca"}, URLs)
 }
 
+// SOLUTION:
 // func Delete(url string) bool {
 // 	var i int
 // 	var u string
@@ -107,7 +116,7 @@ func TestDeleteReturnsTrueIfDeleteIsPerformed(t *testing.T) {
 	assert.True(t, deleted)
 }
 
-// same as previous
+// solution same as previous
 func TestDeleteReturnsFalseIfDeleteIsNotPerformed(t *testing.T) {
 	URLs = []string{}
 	deleted := Delete("http://www.google.ca")
@@ -116,6 +125,7 @@ func TestDeleteReturnsFalseIfDeleteIsNotPerformed(t *testing.T) {
 	assert.False(t, deleted)
 }
 
+// SOLUTION:
 // import "errors"
 
 // func Add(url string) (bool, error) {
@@ -137,7 +147,7 @@ func TestAddReturnsFalseAndErrorIfURLAlreadyExists(t *testing.T) {
 	assert.Equal(t, err.Error(), "url already exists.")
 }
 
-// Same as previous
+// Solution same as previous
 func TestAddReturnsTrueAndNilIfURLIsUnique(t *testing.T) {
 	URLs = []string{}
 	added, err := Add("http://www.google.ca")
