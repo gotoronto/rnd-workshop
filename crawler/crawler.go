3a757b23db932f98f39587188dea2ef9768a300e
Tim Anema <tim.anema@shopify.com>
2018-07-05 12:25:09 -0400

    making tests fail

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -2,3 +2,11 @@ package crawler
 
 // URLs is the array of urls to add and remove urls to
 var URLs []string
+
+// Write a function Add that accepts a url string as a parameter. It will return a
+// boolean if the url was successfully added to the URLs array, it will also return
+// an error if the url already exists in the URLs array
+
+// Write a function Delete that accepts a url string as a parameter. It will return a
+// boolean if the url was successfully found and removed from the URLs array and
+// false otherwise
--- crawler/crawler_test.go
+++ crawler/crawler_test.go
@@ -21,68 +21,68 @@ import (
 // TEST CASES START HERE
 // =============================================
 
-// func TestAddANewURL(t *testing.T) {
-// 	URLs = []string{}
-// 	Add("http://www.google.ca")
-// 
-// 	assert.Equal(t, []string{"http://www.google.ca"}, URLs)
-// }
+func TestAddANewURL(t *testing.T) {
+	URLs = []string{}
+	//Add("http://www.google.ca")
 
-// func TestAddOnlyKeepsUniqueURL(t *testing.T) {
-// 	URLs = []string{}
-// 	Add("http://www.google.ca")
-// 	Add("http://www.google.ca")
+	assert.Equal(t, []string{"http://www.google.ca"}, URLs)
+}
 
-// 	assert.Equal(t, []string{"http://www.google.ca"}, URLs)
-// }
+func TestAddOnlyKeepsUniqueURL(t *testing.T) {
+	URLs = []string{}
+	//Add("http://www.google.ca")
+	//Add("http://www.google.ca")
 
-// // doc on deleting an element from an array: https://github.com/golang/go/wiki/SliceTricks#delete
-// func TestDeleteURL(t *testing.T) {
-// 	URLs = []string{}
-// 	Add("http://www.google.ca")
-// 	Delete("http://www.google.ca")
+	assert.Equal(t, []string{"http://www.google.ca"}, URLs)
+}
 
-// 	assert.Equal(t, []string{}, URLs)
-// }
+// doc on deleting an element from an array: https://github.com/golang/go/wiki/SliceTricks#delete
+func TestDeleteURL(t *testing.T) {
+	URLs = []string{}
+	//Add("http://www.google.ca")
+	//Delete("http://www.google.ca")
 
-// func TestDeleteDoesNothingIfURLNotExist(t *testing.T) {
-// 	Add("http://www.amazon.ca")
-// 	Delete("http://www.google.ca")
+	assert.Equal(t, []string{}, URLs)
+}
 
-// 	assert.Equal(t, []string{"http://www.amazon.ca"}, URLs)
-// }
+func TestDeleteDoesNothingIfURLNotExist(t *testing.T) {
+	//Add("http://www.amazon.ca")
+	//Delete("http://www.google.ca")
 
-// func TestDeleteReturnsTrueIfDeleteIsPerformed(t *testing.T) {
-// 	URLs = []string{}
-// 	Add("http://www.google.ca")
-// 	deleted := Delete("http://www.google.ca")
+	assert.Equal(t, []string{"http://www.amazon.ca"}, URLs)
+}
 
-// 	assert.Equal(t, []string{}, URLs)
-// 	assert.True(t, deleted)
-// }
+func TestDeleteReturnsTrueIfDeleteIsPerformed(t *testing.T) {
+	URLs = []string{}
+	//Add("http://www.google.ca")
+	//deleted := Delete("http://www.google.ca")
 
-// func TestDeleteReturnsFalseIfDeleteIsNotPerformed(t *testing.T) {
-// 	URLs = []string{}
-// 	deleted := Delete("http://www.google.ca")
+	assert.Equal(t, []string{}, URLs)
+	//assert.True(t, deleted)
+}
 
-// 	assert.Equal(t, []string{}, URLs)
-// 	assert.False(t, deleted)
-// }
+func TestDeleteReturnsFalseIfDeleteIsNotPerformed(t *testing.T) {
+	URLs = []string{}
+	//deleted := Delete("http://www.google.ca")
 
-// func TestAddReturnsFalseAndErrorIfURLAlreadyExists(t *testing.T) {
-// 	URLs = []string{"http://www.google.ca"}
-// 	added, err := Add("http://www.google.ca")
+	assert.Equal(t, []string{}, URLs)
+	//assert.False(t, deleted)
+}
 
-// 	assert.Equal(t, []string{"http://www.google.ca"}, URLs)
-// 	assert.False(t, added)
-// 	assert.Equal(t, err.Error(), "url already exists.")
-// }
+func TestAddReturnsFalseAndErrorIfURLAlreadyExists(t *testing.T) {
+	URLs = []string{"http://www.google.ca"}
+	//added, err := Add("http://www.google.ca")
 
-// func TestAddReturnsTrueAndNilIfURLIsUnique(t *testing.T) {
-// 	URLs = []string{}
-// 	added, err := Add("http://www.google.ca")
+	assert.Equal(t, []string{"http://www.google.ca"}, URLs)
+	//assert.False(t, added)
+	//assert.Equal(t, err.Error(), "url already exists.")
+}
 
-// 	assert.Equal(t, []string{"http://www.google.ca"}, URLs)
-// 	assert.True(t, added)
-// 	assert.Nil(t, err)
-// }
+func TestAddReturnsTrueAndNilIfURLIsUnique(t *testing.T) {
+	URLs = []string{}
+	//added, err := Add("http://www.google.ca")
+
+	assert.Equal(t, []string{"http://www.google.ca"}, URLs)
+	//assert.True(t, added)
+	//assert.Nil(t, err)
+}
