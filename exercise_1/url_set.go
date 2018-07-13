51db68c07462de8635063f43809054697835411a
Tim Anema <tim.anema@shopify.com>
2018-07-13 10:04:29 -0400

    Reorganize exercise solutions so that solutions are tucked away

--- exercise_1/url_set.go
+++ exercise_1/url_set.go
@@ -1,4 +1,4 @@
-package crawler
+package urlset
 
 // URLs is the array of urls to add and remove urls to
 var URLs []string
--- exercise_2_solution/crawler.go
+++
@@ -1,7 +0,0 @@
-package urlset
-
-var List = NewURLSet("")
-
-func Add(url string) (bool, error) {
-	return List.Add(url)
-}
